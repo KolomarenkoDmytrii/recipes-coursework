4d3f02b6e6af2c11f62faefb51b38e9cd7c71d97
KolomarenkoDmytrii <dmytrii_kolomarenko@knu.ua>
2024-12-17 20:03:55 +0200

    Started implementing authentification

--- recipesmanagement/urls.py
+++ recipesmanagement/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path("", include("main.urls")),
+    path("user/", include("users.urls")),
     path("admin/", admin.site.urls),
 ]
