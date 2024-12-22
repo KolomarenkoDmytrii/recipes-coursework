9e7df0aac066a0802e0db320d5a91a8326699d39
KolomarenkoDmytrii <dmytrii_kolomarenko@knu.ua>
2024-12-22 13:03:53 +0200

    Add recipe creation form

--- main/models.py
+++ main/models.py
@@ -6,6 +6,7 @@ class Recipe(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=128)
     description = models.TextField()
+    # in minutes
     cooking_time = models.PositiveIntegerField()
     category = models.CharField(max_length=128)
     created_at = models.DateTimeField(auto_now_add=True)
--- main/views.py
+++ main/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 
 from .models import Recipe, RecipeIngredient, RecipeStep, RecipeTag
 
+
 class RecipeForm(ModelForm):
     class Meta:
         model = Recipe
@@ -13,21 +14,47 @@ class RecipeForm(ModelForm):
         labels = {
             "name": "Назва страви",
             "description": "Опис",
-            "cooking_time": "Час приготування",
+            "cooking_time": "Час приготування, хв",
             "category": "Категорія",
         }
+
+
+class RecipeIngredientForm(ModelForm):
+    class Meta:
+        model = RecipeIngredient
+        fields = ["name", "volume", "volume_measure"]
+
+
+class RecipeStepForm(ModelForm):
+    class Meta:
+        model = RecipeStep
+        fields = ["step_description"]
+
+
+class RecipeTagForm(ModelForm):
+    class Meta:
+        model = RecipeTag
+        fields = ["tag_text"]
+
 
 @login_required
 def create_recipe(request):
     if request.method == "POST":
+        context = {}
         recipe_form = RecipeForm(request.POST)
-        recipe = recipe_form.save(commit=False)
-        recipe.user = request.user
-        recipe.save()
+        context["recipe_form"] = recipe_form
+        is_error = False
+        
+        if not recipe_form.is_valid():
+            context["info_error_message"] = "Помилка в інформації про рецепт"
+            is_error = True
 
         steps = request.POST.getlist("step_description")
-        for step_number, step in enumerate(steps):
-            RecipeStep.objects.create(recipe=recipe, step_number=step_number, step_description=step)
+        for step in steps:
+            if not RecipeStepForm({"step_description": step}).is_valid():
+                context["step_error_message"] = "Помилка в описі кроків рецепту"
+                is_error = True
+                break
 
         ingredients = zip(
             request.POST.getlist("ingredient_name"),
@@ -35,15 +62,38 @@ def create_recipe(request):
             request.POST.getlist("ingredient_volume_measure"),
         )
         for name, volume, measure in ingredients:
-            RecipeIngredient.objects.create(recipe=recipe, name=name, volume=volume, volume_measure=measure)
+            if not RecipeIngredientForm({"name": name, "volume": volume, "volume_measure": measure}).is_valid():
+                context["ingredient_error_message"] = "Помилка в описі інгредієнта"
+                is_error = True
+                break
 
         tags = request.POST.getlist("tag_text")
         for tag in tags:
-            RecipeTag.objects.create(recipe=recipe, tag_text=tag)
+            if not RecipeIngredientForm({"tag_text": tag}).is_valid():
+                context["tag_error_message"] = "Помилка в заданні тегу"
+                is_error = True
+                break
 
-        # return redirect('recipe_detail', pk=recipe.pk)
-        return redirect("home")
+        if is_error:
+            print(context)
+            return render(request, "main/create_recipe.html", context)
+        else: # save data if no validation errors occured
+            recipe = recipe_form.save(commit=False)
+            recipe.user = request.user
+            recipe.save()
+
+            for step_number, step in enumerate(steps):
+                RecipeStep.objects.create(recipe=recipe, step_number=step_number, step_description=step)
+
+            for name, volume, measure in ingredients:
+                RecipeIngredient.objects.create(
+                    recipe=recipe, name=name, volume=volume, volume_measure=measure
+                )
+
+            for tag in tags:
+                RecipeTag.objects.create(recipe=recipe, tag_text=tag)
+
+            return render(request, "main/create_recipe_success.html")
     else:
         recipe_form = RecipeForm()
         return render(request, "main/create_recipe.html", {"recipe_form": recipe_form})
-        
