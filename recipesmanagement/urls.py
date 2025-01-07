2950e554027b4eb92e19502df2e149e14808c4bc
KolomarenkoDmytrii <dmytrii_kolomarenko@knu.ua>
2025-01-07 17:29:56 +0200

    Add navbar to templates

--- recipesmanagement/urls.py
+++ recipesmanagement/urls.py
@@ -24,7 +24,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("", include("main.urls")),
-    # path("user/", include("users.urls")),
     path("accounts/", include("accounts.urls")),
     # https://docs.djangoproject.com/en/5.1/topics/auth/default/#module-django.contrib.auth.views
     path("accounts/", include("django.contrib.auth.urls")),
